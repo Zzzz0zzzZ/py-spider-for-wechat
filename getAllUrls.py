3a3a7259b94144099044985ea1272fd96ca2bd43
wsz-sinoiov <1175690167@qq.com>
2022-08-08 15:46:44 +0800

    add regexSearch & write rows to csv

--- getAllUrls.py
+++ getAllUrls.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 # cookie注意更新
 headers = {
-    "cookie": "ua_id=HvjnK6CPHdz8Zt8LAAAAAOhen6ItkIZVMBtW_LgGBJI=; wxuin=59663835389206; cert=8usVhBJvV_bhiOGzvEr5KNZrBLVPpLvI; sig=h01f5bf100266357f9042120ae1f1e6812f0dd97700fa5cc0af9d3206ec20ce8d58f65c6dfd9ddf258b; master_key=oqWPTPRNLKNRxZ933GO+CKIV6t0+ii+8093Le9A8ayE=; rewardsn=; wxtokenkey=777; wwapp.vid=; wwapp.cst=; wwapp.deviceid=; uuid=9a488fb0b902debc7c27e896a8cad45a; rand_info=CAESIJrdL2xSqHo7JtTHuM8d4zAMoSNjxacqc6VQsR4g87rR; slave_bizuin=3940396966; data_bizuin=3940396966; bizuin=3940396966; data_ticket=GYSHLkTsYGcdfdQ/Oj2wGnYGGBkKpTBgA59H5y7Zb2Su8NHcYn40uu+pALruIHzO; slave_sid=VnNCREhCQ1diTkoyQ091ejQ0ckoxdDBiTUxLMWxURHFPVDNsWmFhZVY0UjFzemc0UjhZc19hOERZY2tGa2dEcFRmeEJjV2tWRXRoZlFfMjFlUmhkbXRUdF9pdzUxTEVSVzQxbmhaYnVMMnNuSm55b0NsYkJwOHhLSEdOMk9mYk1hSW5DcDFsZTFjVEw5YUkw; slave_user=gh_495d307185e5; xid=1f54bfb9bf5268fecf62675424ba6a66; mm_lang=zh_CN",
+    "cookie": "ua_id=HvjnK6CPHdz8Zt8LAAAAAOhen6ItkIZVMBtW_LgGBJI=; wxuin=59663835389206; rand_info=CAESIJrdL2xSqHo7JtTHuM8d4zAMoSNjxacqc6VQsR4g87rR; slave_bizuin=3940396966; data_bizuin=3940396966; bizuin=3940396966; data_ticket=GYSHLkTsYGcdfdQ/Oj2wGnYGGBkKpTBgA59H5y7Zb2Su8NHcYn40uu+pALruIHzO; slave_sid=VnNCREhCQ1diTkoyQ091ejQ0ckoxdDBiTUxLMWxURHFPVDNsWmFhZVY0UjFzemc0UjhZc19hOERZY2tGa2dEcFRmeEJjV2tWRXRoZlFfMjFlUmhkbXRUdF9pdzUxTEVSVzQxbmhaYnVMMnNuSm55b0NsYkJwOHhLSEdOMk9mYk1hSW5DcDFsZTFjVEw5YUkw; slave_user=gh_495d307185e5; xid=1f54bfb9bf5268fecf62675424ba6a66; mm_lang=zh_CN",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 }
 url = 'https://mp.weixin.qq.com/cgi-bin/appmsg'
@@ -32,7 +32,7 @@ def getAllUrl(page_num=1):                             # pages
                 'f': 'json',
                 'ajax': '1',
             }
-            time.sleep(random.randint(1, 5))
+            time.sleep(random.randint(1, 8))
             r = requests.get(url, headers=headers, params=data)
             # 解析json
             dic = r.json()
@@ -55,7 +55,7 @@ def write2csv(data_list):
 
 
 if __name__ == '__main__':
-    Page_Num = 20
+    Page_Num = int(input("please input the [Page-Num] you need:\n"))
     start = time.time()
     title, link, update_time = getAllUrl(page_num=Page_Num)
     # save urls, titles, update_times
---
+++ getTitleByKeywords.py
@@ -0,0 +1,62 @@
+import csv
+import re
+
+def getTitleList(titles_storage_path):
+    with open(titles_storage_path, 'r', encoding='utf-8-sig') as csvfile:
+        reader = csv.reader(csvfile)
+        titles_list = [row for row in reader]
+    return titles_list
+
+def regexTitleByKeywords(sentences_list, keywords_list):
+    '''
+    :param sentences_list: 待匹配句子列表
+    :param keywords_list:  匹配关键词列表
+    :return: 匹配成功的句子下标
+    '''
+    res = []
+    for index, sentence in enumerate(sentences_list):
+        pattern = re.compile('|'.join(keywords_list))
+        result_findall = pattern.findall(sentence[0])   # .match()-匹配开头  .search()-只匹配第一个  .findall()-匹配所有
+        if result_findall:                              # .match() / .search() 返回None    .findall() 返回空列表
+            res.append(index)
+    return res
+
+if __name__ == "__main__" :
+    # keyword list
+    keyword_list = ["日", "节", "新年", "快乐", "祝"]
+    # Path
+    title_path = "./data/20pages_title.csv"
+    update_time_path = "./data/20pages_real-time.csv"
+    url_path = "./data/20pages_link.csv"
+    content_path = "./data/20pages_content.csv"
+    # load files
+    title_list = getTitleList(title_path)
+    update_time_list = getTitleList(update_time_path)
+    url_list = getTitleList(url_path)
+    content_list = getTitleList(content_path)
+    # get index
+    res_index = regexTitleByKeywords(title_list, keyword_list)
+
+    data_list = []
+    for a, b, c, d in zip(update_time_list, title_list, url_list, content_list):
+        x = {}
+        x['时间'] = a[0]
+        x['标题'] = b[0]
+        x['地址'] = c[0]
+        x['内容'] = d[0]
+        data_list.append(x)
+
+    file_name = input("please name the new csv-file\n")
+    with open('./filtered_data/' + file_name + '.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['时间', '标题', '地址', '内容'])
+        for idx in res_index:
+            writer.writerow(
+                data_list[idx].values()
+            )
+
+    print("done")
+
+
+
+
